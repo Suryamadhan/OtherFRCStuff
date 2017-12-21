86663e0ad083feedf5d3598b946c0f7069af6d6f
Surya <suryamadhan@gmail.com>
2017-12-21 10:57:52 -0800

    vision code update

--- Bision/src/org/usfirst/frc/team3647/robot/Robot.java
+++ Bision/src/org/usfirst/frc/team3647/robot/Robot.java
@@ -2,14 +2,42 @@ package org.usfirst.frc.team3647.robot;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.vision.VisionRunner;
+import edu.wpi.first.wpilibj.vision.VisionThread;
 
 public class Robot extends IterativeRobot 
 {	
+	
+	private static final int IMG_WIDTH = 320;
+	private static final int IMG_HEIGHT = 240;
+	private VisionThread visionThread;
+	private double centerX = 0.0;
+	private final Object imgLock = new Object();
+	
 	//This function is run whenever the robot starts. This function is used for any initialization of code
 	@Override
 	public void robotInit() 
 	{
-
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture("cam0", 0);
+		camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
+		visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> 
+		{
+			if (!pipeline.filterContoursOutput().isEmpty()) 
+			{
+				Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+				synchronized (imgLock) 
+				{
+					centerX = r.x + (r.width / 2);
+				}
+			}
+		});
+		visionThread.start();
 	}
 
 	 //This function runs once, right before autonomous period starts. 
@@ -24,7 +52,12 @@ public class Robot extends IterativeRobot
 	@Override
 	public void autonomousPeriodic() 
 	{
-
+		double centerX;
+		synchronized (imgLock) {
+		centerX = this.centerX;
+		}
+		double turn = centerX - (IMG_WIDTH / 2);
+		System.out.println("Turn value: " + turn);
 	}
 
 	//This is the function that is called during the Tele-operated period
