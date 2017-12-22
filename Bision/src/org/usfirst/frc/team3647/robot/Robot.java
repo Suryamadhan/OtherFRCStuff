e5e1d51d33c74b8c039ca8db3449c9c6ce24eadb
Surya Madhan <suryamadhan@gmail.com>
2017-12-22 15:40:15 -0800

    test on windows

--- Bision/src/org/usfirst/frc/team3647/robot/Robot.java
+++ Bision/src/org/usfirst/frc/team3647/robot/Robot.java
@@ -23,6 +23,7 @@ public class Robot extends IterativeRobot
 	@Override
 	public void robotInit() 
 	{
+		System.out.println();
 		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture("cam0", 0);
 		camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
 		visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> 
