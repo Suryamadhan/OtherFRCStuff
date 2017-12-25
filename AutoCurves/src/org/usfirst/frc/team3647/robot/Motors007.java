c436edf8bf90fe847fc037d3f1cf1dab5a0b5e2f
Surya <suryamadhan@gmail.com>
2017-12-24 20:38:18 -0800

    code upload

---
+++ AutoCurves/src/org/usfirst/frc/team3647/robot/Motors007.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3647.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Motors007 
+{
+	public static Spark leftTalon = new Spark(2);
+	public static Spark rightTalon =new Spark(1);
+}
