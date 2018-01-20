67db7a2f0e8e8e2cbfece9f8895addfb74cb1945
Surya Madhan <suryamadhan@gmail.com>
2018-01-20 12:44:10 -0800

    upl

--- 2018-BowerUp/src/org/usfirst/frc/team3647/robot/Robot.java
+++ 2018-BowerUp/src/org/usfirst/frc/team3647/robot/Robot.java
@@ -67,8 +67,8 @@ public class Robot extends IterativeRobot {
 			CrashChecker.logTeleopPeriodic();
 			enc.setEncoderValues();
 			joy.setMainContollerValues();
-//			Drivetrain.arcadeDrive(enc.leftEncoderValue, enc.rightEncoderValue, joy.leftJoySticky, joy.rightJoyStickx);
-			Drivetrain.testPID(joy.leftJoySticky, joy.rightJoyStickx);
+			Drivetrain.arcadeDrive(enc.leftEncoderValue, enc.rightEncoderValue, joy.leftJoySticky, joy.rightJoyStickx);
+			//Drivetrain.testPID(joy.leftJoySticky, joy.rightJoyStickx);
 		}
 		catch(Throwable t)
 		{
