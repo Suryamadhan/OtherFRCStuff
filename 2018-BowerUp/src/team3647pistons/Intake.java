115f39817dabe43519d01827f30ee8222cba5337
Tekunalogy <kunal7@hotmail.com>
2018-02-12 09:06:40 -0800

    added methods and their respective statements
    Added methods, again it is copypasta. I don't know if there is more to be added. Lemme know.

--- 2018-BowerUp/src/team3647pistons/Intake.java
+++ 2018-BowerUp/src/team3647pistons/Intake.java
@@ -1,16 +1,30 @@
 package team3647pistons;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Intake 
 {
 	//Double Pistons
+	public static DoubleSolenoid piston = new DoubleSolenoid(1,2);
 	
 	public static void openIntake()
 	{
-		
+		piston.set(DoubleSolenoid.Value.kForward);
 	}
 	
 	public static void closeIntake()
 	{
-		
+		piston.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	public static void runIntake(boolean joyValue)
+	{
+		if(joyValue)
+		{
+			openIntake();
+		}
+		else
+		{
+			closeIntake();
+		}
 	}
 }
