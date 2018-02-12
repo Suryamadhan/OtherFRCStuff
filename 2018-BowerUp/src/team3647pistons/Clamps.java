25a96240ff32f8334fa4ce56683bd90940dfc1ca
Tekunalogy <kunal7@hotmail.com>
2018-02-12 09:03:53 -0800

    Added methods.
    It is literally copypasta from tutorial. There may be more methods to make IDK.

--- 2018-BowerUp/src/team3647pistons/Clamps.java
+++ 2018-BowerUp/src/team3647pistons/Clamps.java
@@ -1,17 +1,32 @@
 package team3647pistons;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Clamps 
 {
 	//Single Piston
 	
+	public static Solenoid piston = new Solenoid(1); //input number may be diff
+	
 	public static void clampThePiston()
 	{
-		
+		piston.set(true);
 	}
 	
 	public static void unClampThePiston()
 	{
-		
+		piston.set(false);
 	}
-
+	
+	//The function we will be calling in Robot.java and Auto
+	public static void runPiston(boolean joyValue)
+	{
+		if(joyValue)
+		{
+			clampThePiston();
+		}
+		else
+		{
+			unClampThePiston();
+		}
+	}
 }
