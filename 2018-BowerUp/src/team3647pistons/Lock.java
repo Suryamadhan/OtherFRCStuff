b91551286543dd57ca078944ea8e5f4fc084b141
farrahkaiyom <32970953+farrahkaiyom@users.noreply.github.com>
2018-02-16 18:25:38 -0800

    update lock piston
    IQ2

--- 2018-BowerUp/src/team3647pistons/Lock.java
+++ 2018-BowerUp/src/team3647pistons/Lock.java
@@ -1,16 +1,36 @@
 package team3647pistons;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 public class Lock 
 {
 	//Double Pistons
+	boolean IQ2 = false;
+	public static Solenoid piston = new Solenoid(5);
 	
 	public static void lock()
 	{
-		
+		piston.set(true);
 	}
 	
 	public static void unLock()
 	{
-		
+		piston.set(false);
 	}
+	
+	public static void runPiston(boolean joyValue)
+	{
+		if(joyValue)
+		{
+			if(!IQ2)
+			{
+				lock();
+				IQ2 = false;
+			}
+			else
+			{
+				unLock();
+				IQ2 = true;
+			}
+		}
 }
