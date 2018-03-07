212dd0cce69e838f94ee1164a51d1430c166d417
gabrielsong9674 <gracegabrielson@icloud.com>
2018-03-07 15:38:16 -0800

    update forks

--- 2018-BowerUp/src/team3647pistons/Forks.java
+++ 2018-BowerUp/src/team3647pistons/Forks.java
@@ -1,11 +1,30 @@
+
 package team3647pistons;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 
-public class Forks 
-{
-
+public class Forks{ 
+	double pistonValue = piston.get();
+	public static DoubleSolenoid piston = new DoubleSolenoid(1, 2);
+	
+	public static void openPiston() {
+		piston.set(DoubleSolenoid.Value.kForward);
+	}
+	public static void closePiston() {
+		piston.set(DoubleSolenoid.Value.kReverse);
+	}
+	public static void runPistons(boolean joyvalue) {
+		if(joyvalue) {
+			if(pistonValue = DoubleSolenoid.Value.kReverse)
+			openPiston();
+		}
+		else {
+			closePiston();
+		}
+	}
 }
+
+
 
